8033d351b479e013b52f653776e25664fa016bb1
daravus <reddaravus@gmail.com>
2021-12-28 14:54:51 +1000

    added preview element, pagination styles

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,13 @@ function elementLoader() {
     $('.item').load('../elements/item.html');
     $('header').load('../elements/header.html');
     $('.sidebar').load('../elements/sidebar.html');
+    $('.preview').load('../elements/preview.html');
 }
 
 $(function() {
     elementLoader();
 });
+
+function openPreview() {
+    // $(#show).style="display: block;"
+}
