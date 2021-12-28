e0a42446a118f62b09b2c43a12c482e313491b9b
daravus <reddaravus@gmail.com>
2021-12-28 12:06:09 +1000

    styling restructure

--- scripts/index.js
+++ scripts/index.js
@@ -2,4 +2,6 @@ console.log('hello')
 
 $(function() {
     $('.item').load('../elements/item.html');
+    $('header').load('../elements/header.html');
+    $('.sidebar').load('../elements/sidebar.html');
 });
